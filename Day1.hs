ce39bb8f9cd4a58c6d78e91e1873450fee82d532
Bartosz Milewski <bartosz@relisoft.com>
2019-12-03 12:34:07 -0800

    Change input file name

--- Day1.hs
+++ Day1.hs
@@ -8,7 +8,7 @@ fuel m = m `div` 3 - 2
 fuelTot m = sum $ takeWhile (> 0) $ iterate fuel (fuel m)
   
 main = do
-  text <- readFile "Data1"
+  text <- readFile "Data1.txt"
   let ms = fmap readInt $ lines text
   print $ sum $ fmap fuel ms
   print $ sum $ fmap fuelTot ms
